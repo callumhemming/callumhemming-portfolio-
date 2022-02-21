b0b1d3ff68dd1a3920c3e1d93317e671c938606b
callumhemming <82006291+callumhemming@users.noreply.github.com>
2022-02-21 21:03:10 +0000

    starting over

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
-import React,{useState, useEffect} from "react"
-import Splash from "../components/Splash";
-
-import Profile from "../components/Profile";
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 
-
-export default function Home({name}) {
-console.log(name)
-
+export default function Home() {
   return (
-    <>
-
-    {/* <Splash /> */}
-    <h1>{name.results[0].name.first}</h1>
-    <Profile />
-    {/* <Github /> */}
-    {/* <Blog /> */}
-    
-    </>
-
+    <></>
   )
-}
-
-export async function getStaticProps(){
-  const res = await fetch("https://randomuser.me/api")
-  const name = await res.json()
-
-  return {
-      props:{
-          name
-      }
-  }
-
 }
