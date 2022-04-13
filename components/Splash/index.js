cc297891ae6ec2825719b1978d685da1cb0aad09
callumhemming <82006291+callumhemming@users.noreply.github.com>
2022-04-13 21:51:09 +0100

    Responsive chnages

--- components/Splash/index.js
+++ components/Splash/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import style from "./splash.module.css";
 import { Avatar } from "antd";
 import { Typography } from "antd";
+import { useMediaQuery } from "react-responsive";
+import classListify from "../../utils/classListify.js";
 
 export default function Splash() {
   const { Title } = Typography;
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
   return (
     <>
       <div className={style.SplashContainer + " pattern"}>
 
 
         
-          <Avatar className={style.avatar} size={500} src="/me.jpg" />
+          <Avatar className={ classListify([style.avatar],[  ()=>isTabletOrMobile ? style.avatarMobile : style.avatarDesktop  ])  } size={500} src="/me.jpg" />
           <h1 className={style.heading}>Hi! I'm Callum Hemming</h1>
           
        
