3a1f0529712060ca9d6876ce7df4986598a2f199
callumhemming <callum.m.hemming@outlook.com>
2022-04-25 04:11:04 +0100

    minor fixes

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,14 +1,23 @@
 import Burger from "./Burger"
 import DesktopNavBar from "./DesktopNavBar"
+import { useMediaQuery } from "react-responsive";
+import React,{useState, useEffect} from "react"
 
-export default function NavBar({mobile}):JSX.Element{
+export default function NavBar():JSX.Element{
 
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const [isMobile, setIsMobile] = useState(false)
+
+
+  useEffect(()=>{
+    setIsMobile(isTabletOrMobile)
+  },[isTabletOrMobile])
 
   return(<>
   
-  {mobile && <Burger/>}
+  {isMobile && <Burger/>}
 
-  {!mobile && <DesktopNavBar/>}
+  {!isMobile && <DesktopNavBar/>}
   
   </>)
 }
